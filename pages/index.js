c8c8f3d65c2b3fe3700f7d37cd3cb7a007bb6d80
Chauncey Plummer <chaunceyp@tapcxm.com>
2024-08-22 17:04:49 -0400

    trying to host

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,20 @@ import Testimonials from './components/Testimonials'
 import BlogPreview from './components/BlogPreview'
 import CTASection from './components/CTASection'
 const inter = Inter({ subsets: ['latin'] })
+// import { createInstance } from '@adobe/alloy'
+// const alloy = createInstance({ name: 'alloy' })
+// alloy('config', {
+//   datastreamId: '47d03159-2436-43b5-9d1a-1f851cea1168',
+//   orgId: '18F332CC5B4DB4150A495DF0@AdobeOrg',
+// })
+// alloy('sendEvent', {
+//   data: { email: 'chaunceyp@tapcxm.com' },
+//   documentUnloading: false,
+//   edgeConfigOverrides: { datastreamId: '0dada9f4-fa94-4c9c-8aaf-fdbac6c56287' },
+//   renderDecisions: true,
+//   type: 'commerce.purchases',
+//   xdm: adobeDataLayer.getState(reference),
+// })
 
 export default function Home() {
   return (
